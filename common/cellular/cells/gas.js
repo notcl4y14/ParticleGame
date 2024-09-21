902fa5756b5fb4debe6c04c507d2d61c42254daf
notcl4y14 <notcl4y@gmail.com>
2024-09-21 08:05:49 +0300

    Introduce CellGas

---
+++ common/cellular/cells/gas.js
@@ -0,0 +1,38 @@
+import Cell from "../../../core/cellular/cell.js";
+
+export default class CellGas extends Cell {
+
+	static step (chunk, cell, x, y) {
+		if (y == 0) {
+			return;
+		}
+
+		// const bottomCell = chunk.getCell(x, y + 1);
+		// const bottomLCell = chunk.getCell(x - 1, y + 1);
+		// const bottomRCell = chunk.getCell(x + 1, y + 1);
+
+		const topEmpty = cell.canPass(chunk, x, y - 1);
+		const leftEmpty = cell.canPass(chunk, x - 1, y);
+		const rightEmpty = cell.canPass(chunk, x + 1, y);
+
+		const left = Math.random() < 0.5;
+		
+		if (!topEmpty) {
+
+			if (left && leftEmpty) {
+				chunk.swapCells(x, y, x - 1, y);
+				return;
+			}
+
+			if (!left && rightEmpty) {
+				chunk.swapCells(x, y, x + 1, y);
+				return;
+			}
+			
+			return;
+		}
+
+		chunk.swapCells(x, y, x, y - 1);
+	}
+
+}
